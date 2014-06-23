8960c2f4b76e30e79c5f5ee849eb534761522251
Simon Pinfold <mail@synap5e.info>
2014-06-23 17:57:02 +1200

    Updated test to use new functionality

--- main.py
+++ main.py
@@ -1,14 +1,40 @@
 #!/bin/env python
 
-import random
+import random, ctypes, struct, time
+from hook_internals import *
+
 r = random.Random()
 
 
-def f1(registers):
-	registers.rax=r.randint(0, 5000)
-	return 'aa'
+class Mod1:
+	my_str = ctypes.create_string_buffer(b"This is my string!")
+	call = 0
+	def f1(self, registers):
+		string_address = get_long(registers.rbp-0x28)
+
+		self.call+=1
+		if self.call == 1 and len(ctypes.string_at(string_address)) >= 3:
+			memory[string_address:string_address+3] = b'HAX'
+			registers.rax = 100
+
+			heap_mapping = memory.mappings[2]
+			heap = memory[heap_mapping.start:heap_mapping.end]
+			tesa = heap.find(b'TESATIME')
+			if tesa != -1:
+				memory[heap_mapping.start+tesa:heap_mapping.start+tesa+3] = b'O_o'
+
+			for a in memory.mappings:
+				if a.pathname == '[heap]':
+					print(a)
+
+		else:
+			free(string_address) # the old string address is being overriden, making it dangling
+			set_long(registers.rbp-0x28, new_buffer(b'Hello'))
+
+
+		#import pdb; pdb.set_trace()
 
 
 hooks = {
-	0x400578 : (0x40057f, f1),
+	0x40078d : (0x40079e, Mod1().f1),
 }
--- test.c
+++ test.c
@@ -5,8 +5,30 @@ int foo(){
 	return 42;
 }
 
+char* bar(){
+	char* b = malloc(10);
+	gets(b);
+	return b;
+}
+
+char* baz = "World";
+
 int main(){
 	open(0xbadf00d, 0, 0);
 	printf("%d\n", foo());
+
+	char *n = malloc(32);
+	memcpy(n, "TESTTIME\x00", 9);
+	n[3] = 'A';
+
+	int i;
+	for(i=0;i<2;i++){
+		char *b = bar();
+		printf("%d, %s, %s\n", foo(), b, baz);
+		free(b);
+	}
+
+	puts(n);
+
 	return 0;
 }
