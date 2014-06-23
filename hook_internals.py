b5f65fedfd6aa39799a9e4646488e18712d2f09c
Simon Pinfold <mail@synap5e.info>
2014-06-23 17:55:19 +1200

    Added memory manilulation

--- hook_internals.py
+++ hook_internals.py
@@ -1,5 +1,5 @@
-import struct
-from collections import OrderedDict
+import ctypes, struct, re
+from collections import OrderedDict, namedtuple
 
 class Registers(OrderedDict):
     __register_names = ['rax', 'rbx', 'rcx', 'rdx', 'rsi', 'rdi', 'rbp', 'rsp', 'r8', 'r9', 'r10', 'r11', 'r12', 'r13', 'r14', 'r15', 'eflags']
@@ -31,3 +31,64 @@ class Registers(OrderedDict):
 
     def values(self):
         return list(super(Registers, self).values())
+
+class Memory:
+    def __init__(self):
+        Mapping = namedtuple('Mapping', ['start', 'end', 'perms', 'offset', 'dev', 'inode', 'pathname'])
+        self.mappings = []
+        with open('/proc/self/maps') as f:
+            for l in f.readlines():
+                data = re.split('\s+', l)
+                #print(data)
+                data += [''] * (6-len(data))
+                addr = data[0].split('-')
+                self.mappings.append(Mapping(
+                    int(addr[0], 16),
+                    int(addr[1], 16),
+                    data[1],
+                    int(data[2], 16),
+                    data[3],
+                    int(data[4]),
+                    data[5]))
+
+    def __getitem__(self, idx):
+        if isinstance(idx, slice):
+            if idx.step:
+                raise IndexError('step unsupported')
+            return ctypes.string_at(idx.start, idx.stop-idx.start)
+        else:
+            return ctypes.string_at(idx, 1)
+    def __setitem__(self, idx, bytes):
+        if isinstance(idx, slice):
+            if idx.step:
+                raise IndexError('step unsupported')
+            if idx.stop-idx.start != len(bytes):
+                raise ValueError('slice length not equal value')
+            start = idx.start
+        else:
+            start = idx
+            if len(bytes) != 1:
+                raise ValueError('got more than one byte')
+        cbytes = ctypes.create_string_buffer(bytes)
+        ctypes.memmove(start, ctypes.addressof(cbytes), len(bytes))
+
+
+libc = ctypes.cdll.LoadLibrary('libc.so.6')
+memory = Memory()
+
+def get_long(addr):
+    return struct.unpack('<Q', ctypes.string_at(addr, 8))[0]
+
+def set_long(addr, value):
+    new_address_raw_bytes = struct.pack('<Q', value)
+    new_address_buffer = ctypes.create_string_buffer(new_address_raw_bytes)
+    ctypes.memmove(addr, ctypes.addressof(new_address_buffer), 8)
+
+def new_buffer(bytes):
+    string_buffer = ctypes.create_string_buffer(bytes)
+    addr = libc.malloc(len(bytes)+1)
+    ctypes.memmove(addr, ctypes.addressof(string_buffer), len(bytes)+1)
+    return addr
+
+def free(addr):
+    return libc.free(addr)
