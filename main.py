643f4093bcf2a815ab90999c78d062fa3e0914d7
Simon Pinfold <mail@synap5e.info>
2014-06-24 15:59:11 +1200

    Demo'd new functionality

--- main.py
+++ main.py
@@ -10,10 +10,12 @@ class Mod1:
 	my_str = ctypes.create_string_buffer(b"This is my string!")
 	call = 0
 	def f1(self, registers):
-		string_address = get_long(registers.rbp-0x28)
+		string_address = memory.get_ptr(registers.rbp-0x30)
 
 		self.call+=1
 		if self.call == 1 and len(ctypes.string_at(string_address)) >= 3:
+			#print(memory[string_address:string_address+5])
+
 			memory[string_address:string_address+3] = b'HAX'
 			registers.rax = 100
 
@@ -23,18 +25,44 @@ class Mod1:
 			if tesa != -1:
 				memory[heap_mapping.start+tesa:heap_mapping.start+tesa+3] = b'O_o'
 
-			for a in memory.mappings:
-				if a.pathname == '[heap]':
-					print(a)
+			# for a in memory.mappings:
+			# 	if a.pathname == '[heap]':
+			# 		print(a)
 
-		else:
-			free(string_address) # the old string address is being overriden, making it dangling
-			set_long(registers.rbp-0x28, new_buffer(b'Hello'))
+		elif self.call == 2:
+			memory.free(string_address) # the old string address is being overridden, making it dangling
+			memory.set_ptr(registers.rbp-0x30, memory.new_buffer(b'Hello'))
+
+			spam_array_ptr = memory.get(registers.rbp-0x50, '@5i')
+			print('> the spam array is ', spam_array_ptr)
+			memory.set(registers.rbp-0x50, (2, 3, 5, 7, 11), '@5i')
+
+		elif self.call == 3:
+
+			# set the loop counter back to 0. '@i' specified integer
+			# note that memory.set(..., (0,), '@i') is equivalent
+			memory.set_single(registers.rbp-0x14, 0, '@i')
+
+			# the value at registers.rbp-0x28 holds the pointer to eggs
+			eggs_array_ptr_ptr = memory.get_ptr(registers.rbp-0x28)
+			eggs_array_ptr = memory.get_ptr(eggs_array_ptr_ptr)
+			# eggs_array_ptr now points to the first element of the array
+			eggs_array = memory.get(eggs_array_ptr, '@5i')
+			print('> the eggs array is ', eggs_array)
+
+			# free the old array
+			memory.free(eggs_array_ptr)
+			# and set the pointer to it to point to a new array of 5 signed integers
+			memory.set_ptr(eggs_array_ptr_ptr, memory.new_buffer((-2, -3, -6, -7, -11), '@5i'))
 
 
 		#import pdb; pdb.set_trace()
 
-
-hooks = {
-	0x40078d : (0x40079e, Mod1().f1),
-}
+def hooks():
+	hooks64 = {
+		0x400834 : (0x400845, Mod1().f1),
+	}
+	if struct.calcsize('P') == 4:
+		return hooks32
+	else:
+		return hooks64
--- test.c
+++ test.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 int foo(){
@@ -14,6 +16,7 @@ char* bar(){
 char* baz = "World";
 
 int main(){
+	int i,y;
 	open(0xbadf00d, 0, 0);
 	printf("%d\n", foo());
 
@@ -21,14 +24,26 @@ int main(){
 	memcpy(n, "TESTTIME\x00", 9);
 	n[3] = 'A';
 
-	int i;
-	for(i=0;i<2;i++){
+	int spam[5];
+	int *eggs = malloc(5);
+	for (i=0;i<5;i++){
+		spam[i] = i*100;
+		eggs[i] = i*200;
+	}
+	int** peggs = &eggs;
+
+
+	for(i=0;i<4;i++){
 		char *b = bar();
 		printf("%d, %s, %s\n", foo(), b, baz);
+		for (y=0;y<5;y++)
+			printf("%d, %d, ", spam[y], (*peggs)[y]);
+		putchar('\n');
 		free(b);
 	}
 
 	puts(n);
+
 
 	return 0;
 }
